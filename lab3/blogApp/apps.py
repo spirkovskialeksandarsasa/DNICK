979dc1591b7ed4fc4783f169149832b246a59a9a
spirkovskialeksandarsasa <sasespirkovski@yahoo.com>
2023-06-15 19:50:34 +0200

    Added lab 3 and 4

--- lab3/blogApp/apps.py
+++ lab3/blogApp/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class BlogappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogApp'
+    name = 'lab3.blogApp'
--- lab3/bloglabs/urls.py
+++ lab3/bloglabs/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blogApp import views
+from lab3.blogApp import views
 from django.conf.urls.static import static
 from django.conf import settings
 
